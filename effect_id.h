143eab5af7f87c2dd28d77a9059fe5543f2cf01e
Steinar H. Gunderson <sgunderson@bigfoot.com>
2012-10-02 20:45:08 +0200

    Fix indenting in effect_id.h.

--- effect_id.h
+++ effect_id.h
@@ -2,18 +2,18 @@
 #define _EFFECT_ID_H 1
 
 enum EffectId {
-       // Mostly for internal use.
-       EFFECT_GAMMA_EXPANSION = 0,
-       EFFECT_GAMMA_COMPRESSION,
-       EFFECT_COLOR_SPACE_CONVERSION,
+	// Mostly for internal use.
+	EFFECT_GAMMA_EXPANSION = 0,
+	EFFECT_GAMMA_COMPRESSION,
+	EFFECT_COLOR_SPACE_CONVERSION,
 
-       // Color.
-       EFFECT_LIFT_GAMMA_GAIN,
-       EFFECT_SATURATION,
+	// Color.
+	EFFECT_LIFT_GAMMA_GAIN,
+	EFFECT_SATURATION,
 
-       // Spatial.
-       EFFECT_MIRROR,
-       EFFECT_VIGNETTE,
+	// Spatial.
+	EFFECT_MIRROR,
+	EFFECT_VIGNETTE,
 };
 
 #endif // !defined(_EFFECT_ID_H)
