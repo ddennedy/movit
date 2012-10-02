75c27c449aabb27ed0b028b57b20d70005a6e447
Steinar H. Gunderson <sgunderson@bigfoot.com>
2012-10-03 00:56:02 +0200

    Try to adjust the mip levels to get box blur for free as needed.

--- blur_effect.cpp
+++ blur_effect.cpp
@@ -22,22 +22,34 @@ void BlurEffect::set_uniforms(GLuint glsl_program_num, const std::string &prefix
 {
 	Effect::set_uniforms(glsl_program_num, prefix, sampler_num);
 
-	//glActiveTexture(GL_TEXTURE0);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 3);
-	//check_error();
+	// We only have 15 taps to work with, and we want that to reach out to about 2.5*sigma.
+	// Bump up the mipmap levels (giving us box blurs) until we have what we need.
+	unsigned base_mipmap_level = 0;
+	float adjusted_radius = radius;
+	float pixel_size = 1.0f;
+	while (adjusted_radius * 2.5f > 7.0f) {
+		++base_mipmap_level;
+		adjusted_radius *= 0.5f;
+		pixel_size *= 2.0f;
+	}	
 
-	set_uniform_float(glsl_program_num, prefix, "pixel_offset", 1.0f / 1280.0f);  // FIXME
+	glActiveTexture(GL_TEXTURE0);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, base_mipmap_level);
+	check_error();
+
+	set_uniform_float(glsl_program_num, prefix, "pixel_offset", pixel_size / 1280.0f);  // FIXME
 
 	// Simple Gaussian weights for now.
 	float weight[15], total = 0.0f;
 	for (unsigned i = 0; i < 15; ++i) {
-		float z = (i - 7.0f) / radius;
+		float z = (i - 7.0f) / adjusted_radius;
 		weight[i] = exp(-(z*z));
 		total += weight[i];
 	}
+	printf("[mip level %d] ", base_mipmap_level);
 	for (unsigned i = 0; i < 15; ++i) {
 		weight[i] /= total;
-		printf("%f\n", weight[i]);
+		printf("%f ", weight[i]);
 	}
 	printf("\n");
 	set_uniform_float_array(glsl_program_num, prefix, "weight", weight, 15);
