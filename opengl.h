0d4629f3aa7a9ea1587797577bee7659a7249153
Steinar H. Gunderson <sgunderson@bigfoot.com>
2012-10-07 18:23:34 +0200

    Add missing file opengl.h.

---
+++ opengl.h
@@ -0,0 +1,11 @@
+#ifndef _OPENGL_H
+#define _OPENGL_H 1
+
+// A common place to find OpenGL includes, if your system should have them in weird places.
+
+#define GL_GLEXT_PROTOTYPES 1
+
+#include <GL/gl.h>
+#include <GL/glext.h>
+
+#endif  // !defined(_OPENGL_H)
