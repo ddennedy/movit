fdad0932f08649e69e824ee73a787671a5bc93a5
Steinar H. Gunderson <sgunderson@bigfoot.com>
2012-10-08 15:01:08 +0200

    FlatInput can only auto-expand from linear or sRGB, not e.g. Rec. 601/709 gamma.

--- flat_input.h
+++ flat_input.h
@@ -17,7 +17,10 @@ public:
 	void finalize();
 
 	// TODO: Check that we actually have the required extension.
-	virtual bool can_output_linear_gamma() const { return true; }
+	virtual bool can_output_linear_gamma() const {
+		return (image_format.gamma_curve == GAMMA_LINEAR ||
+		        image_format.gamma_curve == GAMMA_sRGB);
+	}
 
 	std::string output_fragment_shader();
 
