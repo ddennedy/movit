2c8eabbbad9eb99409ef03970cbb0b5b46dda025
Steinar H. Gunderson <sgunderson@bigfoot.com>
2012-10-12 22:59:38 +0200

    Now that we render to an FBO, we can do with a much smaller window for the test.

--- gtest_sdl_main.cpp
+++ gtest_sdl_main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 0);
 	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 0);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	SDL_SetVideoMode(1280, 720, 0, SDL_OPENGL);
+	SDL_SetVideoMode(32, 32, 0, SDL_OPENGL);
 	SDL_WM_SetCaption("OpenGL window for unit test", NULL);
 
 	testing::InitGoogleTest(&argc, argv);
