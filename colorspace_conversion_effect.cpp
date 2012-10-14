4625d0a9601e33201f257c68392c4b1830388a79
Steinar H. Gunderson <sgunderson@bigfoot.com>
2012-10-14 03:12:32 +0200

    Find Y from the xyz and the D65 white point instead of using the luma coefficients from the standards; for sRGB, that was right, but for Rec. 601 it certainly was not. (This was massively fiddly, but in the end became surprisingly simple.)

--- colorspace_conversion_effect.cpp
+++ colorspace_conversion_effect.cpp
@@ -6,14 +6,14 @@
 // Color coordinates from Rec. 709; sRGB uses the same primaries.
 double rec709_x_R = 0.640,  rec709_x_G = 0.300,  rec709_x_B = 0.150;
 double rec709_y_R = 0.330,  rec709_y_G = 0.600,  rec709_y_B = 0.060;
-double rec709_Y_R = 0.2126, rec709_Y_G = 0.7152, rec709_Y_B = 0.0722;
 
 // Color coordinates from Rec. 601. (Separate for 525- and 625-line systems.)
 double rec601_525_x_R = 0.630, rec601_525_x_G = 0.310, rec601_525_x_B = 0.155;
 double rec601_525_y_R = 0.340, rec601_525_y_G = 0.595, rec601_525_y_B = 0.070;
 double rec601_625_x_R = 0.640, rec601_625_x_G = 0.290, rec601_625_x_B = 0.150;
 double rec601_625_y_R = 0.330, rec601_625_y_G = 0.600, rec601_625_y_B = 0.060;
-double rec601_Y_R = 0.299, rec601_Y_G = 0.587, rec601_Y_B = 0.114;
+
+double d65_x = 0.3127, d65_y = 0.3290;
 
 ColorSpaceConversionEffect::ColorSpaceConversionEffect()
 	: source_space(COLORSPACE_sRGB),
@@ -34,38 +34,86 @@ void get_xyz_matrix(ColorSpace space, Matrix3x3 m)
 
 	double x_R, x_G, x_B;
 	double y_R, y_G, y_B;
-	double Y_R, Y_G, Y_B;
 
 	switch (space) {
 	case COLORSPACE_REC_709:  // And sRGB.
 		x_R = rec709_x_R; x_G = rec709_x_G; x_B = rec709_x_B;
 		y_R = rec709_y_R; y_G = rec709_y_G; y_B = rec709_y_B;
-		Y_R = rec709_Y_R; Y_G = rec709_Y_G; Y_B = rec709_Y_B;
 		break;
 	case COLORSPACE_REC_601_525:
 		x_R = rec601_525_x_R; x_G = rec601_525_x_G; x_B = rec601_525_x_B;
 		y_R = rec601_525_y_R; y_G = rec601_525_y_G; y_B = rec601_525_y_B;
-		Y_R = rec601_Y_R;     Y_G = rec601_Y_G;     Y_B = rec601_Y_B;
 		break;
 	case COLORSPACE_REC_601_625:
 		x_R = rec601_625_x_R; x_G = rec601_625_x_G; x_B = rec601_625_x_B;
 		y_R = rec601_625_y_R; y_G = rec601_625_y_G; y_B = rec601_625_y_B;
-		Y_R = rec601_Y_R;     Y_G = rec601_Y_G;     Y_B = rec601_Y_B;
 		break;
 	default:
 		assert(false);
 	}
 
-	// Convert xyY -> XYZ.
-	double X_R, X_G, X_B;
-	X_R = Y_R * x_R / y_R;
-	X_G = Y_G * x_G / y_G;
-	X_B = Y_B * x_B / y_B;
-	
-	double Z_R, Z_G, Z_B;
-	Z_R = Y_R * (1.0f - x_R - y_R) / y_R;
-	Z_G = Y_G * (1.0f - x_G - y_G) / y_G;
-	Z_B = Y_B * (1.0f - x_B - y_B) / y_B;
+	// Recover z = 1 - x - y.
+	double z_R = 1.0 - x_R - y_R;
+	double z_G = 1.0 - x_G - y_G;
+	double z_B = 1.0 - x_B - y_B;
+
+	// Find the XYZ coordinates of D65 (white point for both Rec. 601 and 709),
+	// normalized so that Y=1.
+	double d65_X = d65_x / d65_y;
+	double d65_Y = 1.0;
+	double d65_Z = (1.0 - d65_x - d65_y) / d65_y;
+
+	// We have, for each primary (example is with red):
+	//
+	//   X_R / (X_R + Y_R + Z_R) = x_R
+	//   Y_R / (X_R + Y_R + Z_R) = y_R
+	//   Z_R / (X_R + Y_R + Z_R) = z_R
+	//
+	// Some algebraic fiddling yields (unsurprisingly):
+	//
+	//   X_R = (x_R / y_R) Y_R
+	//   Z_R = (z_R / y_R) Z_R
+	//
+	// We also know that since RGB=(1,1,1) should give us the
+	// D65 illuminant, we must have
+	//
+	//   X_R + X_G + X_B = D65_X
+	//   Y_R + Y_G + Y_B = D65_Y
+	//   Z_R + Z_G + Z_B = D65_Z
+	//
+	// But since we already know how to express Y and Z by
+	// some constant multiple of X, this reduces to
+	//
+	//   k1 Y_R + k2 Y_G + k3 Y_B = D65_X
+	//      Y_R +    Y_G +    Y_B = D65_Y
+	//   k4 Y_R + k5 Y_G + k6 Y_B = D65_Z
+	//
+	// Which we can solve for (Y_R, Y_G, Y_B) by inverting a 3x3 matrix.
+
+	Matrix3x3 temp, inverted;
+	temp[0] = x_R / y_R;
+	temp[3] = x_G / y_G;
+	temp[6] = x_B / y_B;
+
+	temp[1] = 1.0;
+	temp[4] = 1.0;
+	temp[7] = 1.0;
+
+	temp[2] = z_R / y_R;
+	temp[5] = z_G / y_G;
+	temp[8] = z_B / y_B;
+
+	invert_3x3_matrix(temp, inverted);
+	float Y_R, Y_G, Y_B;
+	multiply_3x3_matrix_float3(inverted, d65_X, d65_Y, d65_Z, &Y_R, &Y_G, &Y_B);
+
+	// Now convert xyY -> XYZ.
+	double X_R = temp[0] * Y_R;
+	double Z_R = temp[2] * Y_R;
+	double X_G = temp[3] * Y_G;
+	double Z_G = temp[5] * Y_G;
+	double X_B = temp[6] * Y_B;
+	double Z_B = temp[8] * Y_B;
 
 	m[0] = X_R; m[3] = X_G; m[6] = X_B;
 	m[1] = Y_R; m[4] = Y_G; m[7] = Y_B;
