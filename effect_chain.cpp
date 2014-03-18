ddennedy/movit

commit 6dea8d2a6695a45aa6209425cdcfb4eabf704340

Generate the FBO anew every render.
This fixes an issue where reusing the same EffectChain from two different GL
contexts would cause errors even if they are shared, because FBOs cannot be
shared between contexts (in the ARB extension, anyway).

There might be some negative performance implications to this, but I was unable
to measure any on Intel/Mesa. If this should prove to be a problem in the future,
we could maybe keep one around per context, or have the caller invalidate the
FBO for us.

effect_chain.cpp
@@ -29,7 +29,6 @@ EffectChain::EffectChain(float aspect_nom, float aspect_denom)
 	: aspect_nom(aspect_nom),
 	  aspect_denom(aspect_denom),
 	  dither_effect(NULL),
-	  fbo(0),
 	  num_dither_bits(0),
 	  finalized(false) {}
 
@@ -47,9 +46,6 @@ EffectChain::~EffectChain()
 		glDeleteShader(phases[i]->vertex_shader);
 		glDeleteShader(phases[i]->fragment_shader);
 		delete phases[i];
-	}
-	if (fbo != 0) {
-		glDeleteFramebuffers(1, &fbo);
 	}
 }
 
@@ -1433,8 +1429,6 @@ void EffectChain::finalize()
 	// since otherwise this turns into an (albeit simple)
 	// register allocation problem.
 	if (phases.size() > 1) {
-		glGenFramebuffers(1, &fbo);
-
 		for (unsigned i = 0; i < phases.size() - 1; ++i) {
 			inform_input_sizes(phases[i]);
 			find_output_size(phases[i]);
@@ -1473,6 +1467,7 @@ void EffectChain::render_to_fbo(GLuint dest_fbo, unsigned width, unsigned height
 
 	// Save original viewport.
 	GLuint x = 0, y = 0;
+	GLuint fbo = 0;
 
 	if (width == 0 && height == 0) {
 		GLint viewport[4];
@@ -1499,6 +1494,8 @@ void EffectChain::render_to_fbo(GLuint dest_fbo, unsigned width, unsigned height
 	glLoadIdentity();
 
 	if (phases.size() > 1) {
+		glGenFramebuffers(1, &fbo);
+		check_error();
 		glBindFramebuffer(GL_FRAMEBUFFER, fbo);
 		check_error();
 	}
@@ -1613,5 +1610,13 @@ void EffectChain::render_to_fbo(GLuint dest_fbo, unsigned width, unsigned height
 			Node *node = phases[phase]->effects[i];
 			node->effect->clear_gl_state();
 		}
+	}
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	check_error();
+
+	if (fbo != 0) {
+		glDeleteFramebuffers(1, &fbo);
+		check_error();
 	}
 }

effect_chain.h
@@ -229,8 +229,6 @@ private:
 	Effect *dither_effect;
 
 	std::vector<Input *> inputs;  // Also contained in nodes.
-
-	GLuint fbo;
 	std::vector<Phase *> phases;
 
 	unsigned num_dither_bits;

commit 6aff20a0932dc9b668b77981390539dd710cf06d

Keep FBOs around in EffectChain again.
Seemingly creating and deleting them is crazy expensive on NVidia
(~3 ms for a create/delete pair), so 6dea8d2 caused a performance
regression at high frame rates. Now we instead keep one around per
context (they cannot be shared), which brings us basically back
to where we were performance-wise.

Reported by Christophe Thommeret.

effect_chain.cpp
@@ -59,6 +59,11 @@ EffectChain::~EffectChain()
 	if (owns_resource_pool) {
 		delete resource_pool;
 	}
+	for (map<void *, GLuint>::const_iterator fbo_it = fbos.begin();
+	     fbo_it != fbos.end(); ++fbo_it) {
+		glDeleteFramebuffers(1, &fbo_it->second);
+		check_error();
+	}
 }
 
 Input *EffectChain::add_input(Input *input)
@@ -1405,6 +1410,7 @@ void EffectChain::render_to_fbo(GLuint dest_fbo, unsigned width, unsigned height
 	// Save original viewport.
 	GLuint x = 0, y = 0;
 	GLuint fbo = 0;
+	void *context = get_gl_context_identifier();
 
 	if (width == 0 && height == 0) {
 		GLint viewport[4];
@@ -1424,8 +1430,13 @@ void EffectChain::render_to_fbo(GLuint dest_fbo, unsigned width, unsigned height
 	check_error();
 
 	if (phases.size() > 1) {
-		glGenFramebuffers(1, &fbo);
-		check_error();
+		if (fbos.count(context) == 0) {
+			glGenFramebuffers(1, &fbo);
+			check_error();
+			fbos.insert(make_pair(context, fbo));
+		} else {
+			fbo = fbos[context];
+		}
 		glBindFramebuffer(GL_FRAMEBUFFER, fbo);
 		check_error();
 	}
@@ -1564,11 +1575,6 @@ void EffectChain::render_to_fbo(GLuint dest_fbo, unsigned width, unsigned height
 
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	check_error();
-
-	if (fbo != 0) {
-		glDeleteFramebuffers(1, &fbo);
-		check_error();
-	}
 }
 
 }  // namespace movit

effect_chain.h
@@ -280,6 +280,7 @@ private:
 	std::map<Effect *, Node *> node_map;
 	Effect *dither_effect;
 
+	std::map<void *, GLuint> fbos;  // One for each OpenGL context.
 	std::vector<Input *> inputs;  // Also contained in nodes.
 	std::vector<Phase *> phases;
 

util.cpp
@@ -9,6 +9,14 @@
 #include "init.h"
 #include "util.h"
 
+#if defined(__DARWIN__)
+#include <OpenGL/OpenGL.h>
+#elif defined(WIN32)
+#include <GL/wglew.h>
+#else
+#include <GL/glxew.h>
+#endif
+
 using namespace std;
 
 namespace movit {
@@ -231,6 +239,17 @@ unsigned next_power_of_two(unsigned v)
 	v |= v >> 16;
 	v++;
 	return v;
+}
+
+void *get_gl_context_identifier()
+{
+#if defined(__DARWIN__)
+	return (void *)CGLGetCurrentContext();
+#elif defined(WIN32)
+	return (void *)wglGetCurrentContext();
+#else
+	return (void *)glXGetCurrentContext();
+#endif
 }
 
 }  // namespace movit

util.h
@@ -56,6 +56,12 @@ void cleanup_vertex_attribute(GLuint glsl_program_num, const std::string &attrib
 // If v is not already a power of two, return the first higher power of two.
 unsigned next_power_of_two(unsigned v);
 
+// Get a pointer that represents the current OpenGL context, in a cross-platform way.
+// This is not intended for anything but identification (ie., so you can associate
+// different FBOs with different contexts); you should probably not try to cast it
+// back into anything you intend to pass into OpenGL.
+void *get_gl_context_identifier();
+
 }  // namespace movit
 
 #ifdef NDEBUG