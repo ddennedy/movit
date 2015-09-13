0df63ebb6d5c67be253bfa77ef3960c221ed9d44
Steinar H. Gunderson <sgunderson@bigfoot.com>
2015-09-13 12:09:08 +0200

    Add a version header file to help clients that need to relate to multiple versions of Movit.

---
+++ version.h
@@ -0,0 +1,10 @@
+#ifndef _MOVIT_VERSION_H
+#define _MOVIT_VERSION_H 1
+
+// A number that will increase every time the visible user API
+// changes, even within git versions. There is no specific version
+// documentation outside the regular changelogs, though.
+
+#define MOVIT_VERSION 1
+
+#endif // !defined(_MOVIT_VERSION_H)
