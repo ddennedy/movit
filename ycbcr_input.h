930a54d9a25ad48c2e014cd270797917db51793f
Steinar H. Gunderson <sgunderson@bigfoot.com>
2017-02-19 11:02:09 +0100

    Loosen up some restrictions on YCbCrInput if we have interleaved mode.

--- ycbcr_input.h
+++ ycbcr_input.h
@@ -75,7 +75,8 @@ public:
 	unsigned get_height() const { return height; }
 	Colorspace get_color_space() const { return image_format.color_space; }
 	GammaCurve get_gamma_curve() const { return image_format.gamma_curve; }
-	virtual bool can_supply_mipmaps() const { return false; }
+	virtual bool can_supply_mipmaps() const { return ycbcr_input_splitting == YCBCR_INPUT_INTERLEAVED; }
+	virtual bool is_single_texture() const { return ycbcr_input_splitting == YCBCR_INPUT_INTERLEAVED; }
 
 	// Tells the input where to fetch the actual pixel data. Note that if you change
 	// this data, you must either call set_pixel_data() again (using the same pointer
